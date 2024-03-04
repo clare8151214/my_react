44ce986d4ad604c88d9f2d44fe13b15a5799a383
DESKTOP-HRD8ST1\User <clare8151214@gmail.com>
2024-03-04 21:50:59 +0800

    Updates

---
+++ precache-manifest.9374a28cd39009bcfa00e94e135d2b4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dbda0992d22836ad8dfa707b75c0f8",
+    "url": "/my-react/index.html"
+  },
+  {
+    "revision": "44f33cfc010a7f1022c8",
+    "url": "/my-react/static/js/2.382dbd80.chunk.js"
+  },
+  {
+    "revision": "64e8ff20c61fdabad54b",
+    "url": "/my-react/static/js/main.305d48b7.chunk.js"
+  },
+  {
+    "revision": "cc9278b06e1425773c6c",
+    "url": "/my-react/static/js/runtime~main.9d651b49.js"
+  }
+]);
